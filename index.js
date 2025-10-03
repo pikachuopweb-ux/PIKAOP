9f7a9277d6023d0a6305295f3a783f0705bf2685
pikachuopweb-ux <pikachu.op.web@gmail.com>
2025-10-03 19:34:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+alert("Thanks For Visiting My Website!");
+
+// const white = document.querySelector('.white');
+// white.addEventListener('click', () => {
+//     document.body.style.backgroundColor = 'white';
+//     document.body.style.color = 'black';
+// });
+
+
+// const body = document.querySelector('body');
+// body.style.backgroundColor = 'white';
+// body.style.color = 'black';
+
+// if you are seeing this code, it means you are a developer and you are looking at my code. Hello there! I hope you have a great day! :)   
